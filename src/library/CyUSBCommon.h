2acad7f776e3065b2e4e3b284e72e22b72a10b00
cyrozap <cyrozap@gmail.com>
2014-10-27 21:41:43 -0400

    Moved CyUSBSerial.h to "include" directory

--- src/library/CyUSBCommon.h
+++ src/library/CyUSBCommon.h
@@ -26,7 +26,7 @@
 #include <stdbool.h>
 #include <pthread.h>
 #include <libusb-1.0/libusb.h>
-#include "../../common/header/CyUSBSerial.h"
+#include "../../include/CyUSBSerial.h"
 #pragma pack(1)
 typedef struct CY_DEVICE {
     
