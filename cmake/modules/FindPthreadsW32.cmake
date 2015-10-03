42cb8b7f8c90fb420893364f983f4dafea87b496
cyrozap <cyrozap@gmail.com>
2015-10-02 22:59:08 -0400

    First try at fixing Windows build with pthreads-w32

---
+++ cmake/modules/FindPthreadsW32.cmake
@@ -0,0 +1,46 @@
+# This file finds pthreads-w32 on Windows
+
+if(DEFINED __INCLUDED_LIBCYUSBSERIAL_FINDPTHREADS_CMAKE)
+    return()
+endif()
+set(__INCLUDED_LIBCYUSBSERIAL_FINDPTHREADS_CMAKE TRUE)
+
+include(CheckLibraryExists)
+include(CheckIncludeFile)
+
+set(PTHREADS_LIBRARY_PATH
+    "C:/Program Files/pthreads-w32/prebuilt-dll-2-9-1-release/"
+    CACHE
+    PATH
+    "Path to pthreads-w32 files."
+)
+
+if(CMAKE_CL_64)
+    set(PTHREADS_LIBRARY_PATH_SUFFIX dll/x64)
+else(CMAKE_CL_64)
+    set(PTHREADS_LIBRARY_PATH_SUFFIX dll/x86)
+endif(CMAKE_CL_64)
+
+find_file(PTHREADS_HEADER_FILE
+    NAMES
+    pthread.h
+    PATHS
+    ${PTHREADS_PATH}
+    PATH_SUFFIXES
+    include
+    )
+mark_as_advanced(PTHREADS_HEADER_FILE)
+get_filename_component(PTHREADS_INCLUDE_DIRS "${PTHREADS_HEADER_FILE}" PATH)
+
+find_library(pthread_LIBRARY
+    NAMES
+    pthreadVC2
+    PATHS
+    ${PTHREADS_PATH}
+    PATH_SUFFIXES
+    ${PTHREADS_LIBRARY_PATH_SUFFIX}
+    )
+mark_as_advanced(pthread_LIBRARY)
+if(pthread_LIBRARY)
+    set(PTHREADS_LIBRARIES ${pthread_LIBRARY})
+endif(pthread_LIBRARY)
