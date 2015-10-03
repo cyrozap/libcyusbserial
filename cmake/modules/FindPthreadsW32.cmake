be2e55817e7644d232ff029fb3586b63e87ca92a
cyrozap <cyrozap@gmail.com>
2015-10-02 23:22:10 -0400

    Don't make the include statement conditional

--- cmake/modules/FindPthreadsW32.cmake
+++ cmake/modules/FindPthreadsW32.cmake
@@ -8,50 +8,53 @@ set(__INCLUDED_LIBCYUSBSERIAL_FINDPTHREADS_CMAKE TRUE)
 include(CheckLibraryExists)
 include(CheckIncludeFile)
 
-set(PTHREADS_LIBRARY_PATH
-    "C:/Program Files/pthreads-w32/prebuilt-dll-2-9-1-release/"
-    CACHE
-    PATH
-    "Path to pthreads-w32 files."
-)
-
-if(CMAKE_CL_64)
-    set(PTHREADS_LIBRARY_PATH_SUFFIX dll/x64)
-else(CMAKE_CL_64)
-    set(PTHREADS_LIBRARY_PATH_SUFFIX dll/x86)
-endif(CMAKE_CL_64)
-
-find_file(PTHREADS_HEADER_FILE
-    NAMES
-    pthread.h
-    PATHS
-    ${PTHREADS_PATH}
-    PATH_SUFFIXES
-    include
+# Windows only
+if(WIN32)
+    set(PTHREADS_LIBRARY_PATH
+        "C:/Program Files/pthreads-w32/prebuilt-dll-2-9-1-release/"
+        CACHE
+        PATH
+        "Path to pthreads-w32 files."
     )
-mark_as_advanced(PTHREADS_HEADER_FILE)
-get_filename_component(PTHREADS_INCLUDE_DIRS "${PTHREADS_HEADER_FILE}" PATH)
 
-find_library(pthread_LIBRARY
-    NAMES
-    pthreadVC2
-    PATHS
-    ${PTHREADS_PATH}
-    PATH_SUFFIXES
-    ${PTHREADS_LIBRARY_PATH_SUFFIX}
-    )
-mark_as_advanced(pthread_LIBRARY)
-if(pthread_LIBRARY)
-    set(PTHREADS_LIBRARIES ${pthread_LIBRARY})
-endif(pthread_LIBRARY)
+    if(CMAKE_CL_64)
+        set(PTHREADS_LIBRARY_PATH_SUFFIX dll/x64)
+    else(CMAKE_CL_64)
+        set(PTHREADS_LIBRARY_PATH_SUFFIX dll/x86)
+    endif(CMAKE_CL_64)
 
-if(PTHREADS_INCLUDE_DIRS AND PTHREADS_LIBRARIES)
-    set(PTHREADS_FOUND TRUE)
-endif(PTHREADS_INCLUDE_DIRS AND PTHREADS_LIBRARIES)
+    find_file(PTHREADS_HEADER_FILE
+        NAMES
+        pthread.h
+        PATHS
+        ${PTHREADS_PATH}
+        PATH_SUFFIXES
+        include
+        )
+    mark_as_advanced(PTHREADS_HEADER_FILE)
+    get_filename_component(PTHREADS_INCLUDE_DIRS "${PTHREADS_HEADER_FILE}" PATH)
 
-message(STATUS "Checking for pthreads-w32...")
-if(PTHREADS_FOUND)
-    set(CMAKE_REQUIRED_INCLUDES "${PTHREADS_INCLUDE_DIRS}")
-    check_include_file("{PTHREADS_HEADER_FILE}" PTHREADS_FOUND)
-    message(STATUS "pthreads-w32 found")
-endif(PTHREADS_FOUND)
+    find_library(pthread_LIBRARY
+        NAMES
+        pthreadVC2
+        PATHS
+        ${PTHREADS_PATH}
+        PATH_SUFFIXES
+        ${PTHREADS_LIBRARY_PATH_SUFFIX}
+        )
+    mark_as_advanced(pthread_LIBRARY)
+    if(pthread_LIBRARY)
+        set(PTHREADS_LIBRARIES ${pthread_LIBRARY})
+    endif(pthread_LIBRARY)
+
+    if(PTHREADS_INCLUDE_DIRS AND PTHREADS_LIBRARIES)
+        set(PTHREADS_FOUND TRUE)
+    endif(PTHREADS_INCLUDE_DIRS AND PTHREADS_LIBRARIES)
+
+    message(STATUS "Checking for pthreads-w32...")
+    if(PTHREADS_FOUND)
+        set(CMAKE_REQUIRED_INCLUDES "${PTHREADS_INCLUDE_DIRS}")
+        check_include_file("{PTHREADS_HEADER_FILE}" PTHREADS_FOUND)
+        message(STATUS "pthreads-w32 found")
+    endif(PTHREADS_FOUND)
+endif()
