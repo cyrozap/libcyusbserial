0c5df8a6a2961f801e7fbe7efb7fcc3c46bf33e3
cyrozap <cyrozap@gmail.com>
2015-10-03 17:10:16 -0400

    Download the release archive for eficiency and correct paths

--- cmake/modules/FindLibPThreadsWin32.cmake
+++ cmake/modules/FindLibPThreadsWin32.cmake
@@ -27,7 +27,7 @@ include ( CheckLibraryExists )
 include ( CheckIncludeFile )
 
 set(LIBPTHREADSWIN32_PATH
-        "C:/Program Files (x86)/pthreads-win32"
+        "C:/Program Files/pthreads-win32"
         CACHE
         PATH
         "Path to win-pthreads files. (This is generally only needed for Windows users who downloaded binary distributions.)"
